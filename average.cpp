b23197706b68c201c41519b86b12053746661179
Tai Rodrig <taicapra@gmail.com>
2014-10-17 22:12:04 -0700

    average

--- average.cpp
+++ average.cpp
@@ -24,20 +24,21 @@ while(1){
 
     cvtColor(src,gray,CV_BGR2GRAY);
     //sprintf(name,"gray%4d.jpg",i);
+    Mat newsrc = src(Rect(92,48,640,320));
     //imwrite(name, gray);
 
     //namedWindow("src");
     //imshow("src",src);
     //threshold( gray, gray, 40, 255, 0);
-    images.push_back(src);
+    images.push_back(newsrc);
     //imshow("result",gray);
 
     i+=1;
     //waitKey(0);
 }
 
+
 Mat sum = images[0];
-float x = 1.0;
 for (float k = 1; k < images.size(); k++){
     float K = log2(k);
     float factor = k/(k+1.0);
