d3c6c300a29e51722069cd21c10e62e2dbbdc796
Tai Rodrig <taicapra@gmail.com>
2014-10-18 14:28:19 -0700

    found keypoints

--- average.cpp
+++ average.cpp
@@ -12,57 +12,39 @@ char name[50];
 int i = 2;
 Mat gray;
 std::vector<Mat> images;
+std::vector<KeyPoint> points;
 
 int main(){
-while(1){  
+    while(1){  
+        sprintf(name,"./png_images/%04d.png",i);
+        Mat src= imread(name,1);
+        if(!src.data ) break;
+        cvtColor(src,gray,CV_BGR2GRAY);
+        Mat newsrc = src(Rect(92,48,640,320));
+        threshold( newsrc, newsrc, 50, 255, 0);
+        images.push_back(newsrc);
+        i+=1;
+    }
 
-    sprintf(name,"./png_images/%04d.png",i);
-    Mat src= imread(name,1);
+    /*
+    AVERAGE ALL THE IMAGES
+    adding weighted:
+    1 + 1/2 + 1/3 + 1/4 + ... + 1/n
+    */
+    Mat sum = images[0];
+    for (float k = 1; k < images.size(); k++){
+        float factor = k/(k+1.0);
+        addWeighted( sum, factor, images[k], 1.0-factor, 0.0, sum);
+    }
 
-
-    if(!src.data ) break;
-
-    cvtColor(src,gray,CV_BGR2GRAY);
-    //sprintf(name,"gray%4d.jpg",i);
-    Mat newsrc = src(Rect(92,48,640,320));
-    //imwrite(name, gray);
-
-    //namedWindow("src");
-    //imshow("src",src);
-    threshold( newsrc, newsrc, 50, 255, 0);
-    images.push_back(newsrc);
-    //imshow("result",gray);
-
-    i+=1;
-    //waitKey(0);
-}
-
-
-Mat sum = images[0];
-for (float k = 1; k < images.size(); k++){
-    float K = log2(k);
-    float factor = k/(k+1.0);
-    addWeighted( sum, factor, images[k], 1.0-factor, 0.0, sum);
-    printf("%4f, %f\n",k,factor);
-   
-}
-
-threshold( sum, sum, 80, 255, 0);
- imshow("sum",sum);
+    imwrite("results/average.png", sum);
+    threshold( sum, sum, 80, 255, 0);
+    imwrite("results/average_threshold.png", sum);
+    imshow("sum",sum);
     waitKey(0);
-
-    const char      * wndName = "Source image",
-                                * wndNameGray = "Gray img", 
-                                * wndNameOut = "Out",
-                                * filename = "../data/images/eyebrows-smoothed.bmp";
-
-        Mat src, gray, thresh, binary;
-        Mat out = sum;
-        vector<KeyPoint> keyPoints;
-        vector< vector <Point> > contours;
-        vector< vector <Point> > approxContours;
-
-        SimpleBlobDetector::Params params;
+    Mat out = sum;
+    vector<KeyPoint> keyPoints;
+    SimpleBlobDetector::Params params;
         params.minThreshold = 40;
         params.maxThreshold = 60;
         params.thresholdStep = 5;
@@ -74,29 +56,26 @@ threshold( sum, sum, 80, 255, 0);
         params.maxConvexity = 10;
         params.filterByCircularity = false;
         params.filterByColor = false;
-        namedWindow( wndNameOut, CV_GUI_NORMAL );
+    namedWindow( "Out", CV_GUI_NORMAL );
+    line( sum, Point(0, sum.rows-1), Point( sum.cols-1, sum.rows-1 ), Scalar::all(255) );
 
-        src = imread( filename, CV_LOAD_IMAGE_GRAYSCALE );
-        line( sum, Point(0, sum.rows-1), Point( sum.cols-1, sum.rows-1 ), Scalar::all(255) );
-
-        SimpleBlobDetector blobDetector( params );
-        blobDetector.create("SimpleBlob");
-
-        for(;;)
-        {
-                blobDetector.detect( sum, keyPoints);
+    SimpleBlobDetector blobDetector( params );
+    blobDetector.create("SimpleBlob");
+    blobDetector.detect( sum, keyPoints);
                 //blobDetector.detectEx( src, keyPoints, contours );
-                drawKeypoints( sum, keyPoints, out, CV_RGB(0,keyPoints.size(),0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    drawKeypoints( sum, keyPoints, out, CV_RGB(0,keyPoints.size(),0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
                 //approxContours.resize( contours.size() );
-
-
-                cout << "Keypoints " << keyPoints.size() << endl;
-        
-                imshow( wndNameOut, out );
-                waitKey(0);
-        }
-
-
-
-return 0;
+                /*
+                cout << "{";
+                cout << keyPoints[0].pt;
+                for (i = 1; i < keyPoints.size(); i++){
+                    cout << ", " << keyPoints[i].pt;
+                }
+                cout << "}";
+                */
+    points = keyPoints;
+    imwrite("results/average_keypoints.png", out);
+    imshow( "Out", out);
+    waitKey(0);
+    return 0;
 }
